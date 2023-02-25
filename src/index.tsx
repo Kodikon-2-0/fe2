c65b4245a157c71e1abd18d2ae8ee10947cecd35
Sai Sudhir <saisudhirs@outlook.com>
2023-02-25 11:30:39 +0530

    FEATURE: Repeat commit from fe

---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from './Pages/Login'
+//
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const Root = () => {
+    return <React.StrictMode>
+        {/*<Provider store={store}>*/}
+        {/*        <ThemeProvider theme={}>*/}
+        <BrowserRouter>
+            {/*<App/>*/}
+            <Routes>
+                <Route path={"/"} element={<Login/>}/>
+            </Routes>
+        </BrowserRouter>
+        {/*</ThemeProvider>*/}
+    </React.StrictMode>
+}
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement);
+root.render(
+    <Root/>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

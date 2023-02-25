3493684905415ab487f8fe73587d75527a554c9d
Sai Sudhir <saisudhirs@outlook.com>
2023-02-25 11:45:37 +0530

    WIP: Login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import {Card, Grid, Paper, TextField} from "@mui/material";
 
 export default function Login() {
 
-    return <>
-        Login!
-    </>
+    return <Paper>
+        <Card sx={{padding: 2}}>
+           <Grid container direction={"column"} gap={2} padding={1}>
+                <Grid>
+                    <TextField label={"Username"}></TextField>
+                </Grid>
+               <Grid>
+                    <TextField label={"Password"} inputProps={{type: "password"}}></TextField>
+                </Grid>
+           </Grid>
+        </Card>
+    </Paper>
 }
